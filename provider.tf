87b78341e4b74e103c459ae535a86771601ca690
Saurabh bhatia <32091801+saurabh1611@users.noreply.github.com>
2023-07-28 15:44:05 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,22 +7,27 @@ terraform {
   }
 }
 
-provider "vault" {
-  address = "http://3.133.92.151:8200"
-}
+#provider "vault" {
+#  address = "http://3.133.92.151:8200"
+#}
 
 # Configure the AWS Provider
 provider "aws" {
   region     = "us-east-1"
-  access_key = data.vault_aws_access_credentials.aws.access_key
-  secret_key = data.vault_aws_access_credentials.aws.secret_key
-  token      = data.vault_aws_access_credentials.aws.security_token
+#  access_key = data.vault_aws_access_credentials.aws.access_key
+#  secret_key = data.vault_aws_access_credentials.aws.secret_key
+#  token      = data.vault_aws_access_credentials.aws.security_token
+   access_key = var.AWS_ACCESS_KEY
+   secret_key = var.AWS_SECRET_KEY
 }
 
-data "vault_aws_access_credentials" "aws" {
-  backend = "aws"
-  role    = "my-ec2-role"
-}
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+
+#data "vault_aws_access_credentials" "aws" {
+#  backend = "aws"
+#  role    = "my-ec2-role"
+#}
 
 # Create a VPC
 resource "aws_vpc" "example" {
